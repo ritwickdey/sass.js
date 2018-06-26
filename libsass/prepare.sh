5355c7abb1c2e3829a01574a869188c874572093
Ritwick Dey <ritwickdey@outlook.com>
2018-06-26 10:30:13 +0530

    clone with token

--- libsass/prepare.sh
+++ libsass/prepare.sh
@@ -15,6 +15,14 @@ rm -rf ./libsass
 
 # download
 echo "  downloading repository to target directory"
-git clone https://github.com/sass/libsass.git libsass
+# If Token is not avaliable in env
+if [ -z ${GH_TOKEN+x} ]
+then
+  git clone https://github.com/sass/libsass.git libsass
+else
+  echo "Cloning with Token"
+  git clone https://${GH_TOKEN}@github.com/sass/libsass.git libsass
+fi
+
 echo "  checking out branch/tag and initializing submodules"
 (cd libsass && git checkout ${LIBSASS_VERSION} && git submodule init && git submodule update)
